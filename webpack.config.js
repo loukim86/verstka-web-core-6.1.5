0817a41ba6cf57f8095aa7e3589ba4930b4aa075
Luiza Kim <kimluisa2886@gmail.com>
2023-12-05 15:04:16 +0900

    changed folder

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         clean: true,
         filename: '[name].[contenthash].js'
     },
